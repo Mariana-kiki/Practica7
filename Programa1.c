c263306fb91619a56ce8891bdd298da177ed3bbd
Mariana-kiki <acosmariana9@outlook.com>
2025-10-16 14:37:16 -0600

    Programa1.c
    Tiene una modificación que le hicimos durante la práctica.

--- Programa1.c
+++ Programa1.c
@@ -11,3 +11,18 @@ int main ()
   printf("\tEl programa sigue su flujo.\n");
   return 0;
 }
+
+Modificación que le hcimos durante la práctica:
+#include<stdio.h>
+int main ()
+{
+  int a, b;
+  a = 1;
+  b = 5;
+  if (a > b)
+  {  
+    printf("\ta (%d) es mayor a b (%d).\n",a,b);
+  }
+  printf("\tEl programa sigue su flujo.\n");
+  return 0;
+}
