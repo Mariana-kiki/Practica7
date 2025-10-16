c7f16863411101b19e9d9827447783ac1651c28e
Mariana-kiki <acosmariana9@outlook.com>
2025-10-16 14:39:46 -0600

    Programa2.c
    También tiene una modificación.

--- Programa2.c
+++ Programa2.c
@@ -14,3 +14,21 @@ int main()
     printf("Esta instrucción siempre se ejecuta.\n");
   return 0;
 }
+
+Modificación que le hicimos durante la práctica:
+#include<stdio.h>
+int main()
+{
+  if (!0)
+  {
+    printf("Esta instrucción nunca se ejecuta\n");
+    printf("porque la condición siempre es falsa (0).\n");
+  }
+  if (1) 
+      // El bloque de código de esta estructura if
+      // solo consta de una línea porque los comentarios
+      // no son tomados en cuenta por el compilador.
+      // La condición siempre es verdadera (diferente de 0)
+    printf("Esta instrucción siempre se ejecuta.\n");
+  return 0;
+}
